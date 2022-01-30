da7695fedc52196c58182f618f1555389f27004f
Dimitri Coelho <dimitri@belcaro.uk>
2022-01-30 18:13:41 +0000

    function populateFaces() implemented

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-const colors = ["Y", "R", "W", "G", "B", "O"];
+const colors = ["Y", "W", "B", "G", "R", "O"];
+const faces = [];
+for (let i = 0; i < 6; i++) {
+  faces[i] = ["", "", "", "", colors[i], "", "", "", ""];
+}
+
+let counters = [8, 8, 8, 8, 8, 8];
+const populateFaces = () => {
+  for (let i = 0; i < 6; i++) {
+    for (let j = 0; j < 9; j++) {
+      if (j !== 4) {
+        do {
+          color = Math.floor(Math.random() * 6);
+        } while (counters[color] === 0);
+        faces[i][j] = colors[color];
+        counters[color]--;
+      }
+    }
+  }
+};
+
+populateFaces();
+console.log(faces);
