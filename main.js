5b7a9c8316268b9f1010c415282656ee1afad53e
Dimitri Coelho <dimitri@belcaro.uk>
2022-01-30 18:31:55 +0000

    function turnFace() implemented

--- main.js
+++ main.js
@@ -19,5 +19,32 @@ const populateFaces = () => {
   }
 };
 
+const turnFace = (face, clockwise) => {
+  let newFace = [];
+  newFace[4] = face[4];
+  if (clockwise) {
+    newFace[0] = face[6];
+    newFace[1] = face[3];
+    newFace[2] = face[0];
+    newFace[3] = face[7];
+    newFace[5] = face[1];
+    newFace[6] = face[8];
+    newFace[7] = face[5];
+    newFace[8] = face[2];
+  } else {
+    newFace[0] = face[2];
+    newFace[1] = face[5];
+    newFace[2] = face[8];
+    newFace[3] = face[1];
+    newFace[5] = face[7];
+    newFace[6] = face[0];
+    newFace[7] = face[3];
+    newFace[8] = face[6];
+  }
+  return newFace;
+};
+
 populateFaces();
+console.log(faces);
+faces[5] = turnFace(faces[5], false);
 console.log(faces);
