2019ea238b1e013251ca3b9c172cdc4fa53f949b
Dimitri Coelho <dimitri@belcaro.uk>
2022-01-30 21:47:45 +0000

    Created cube object Factory and moved cube functions to them.

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
-const colors = ["Y", "W", "B", "G", "R", "O"];
-const faces = [];
-for (let i = 0; i < 6; i++) {
-  faces[i] = ["", "", "", "", colors[i], "", "", "", ""];
-}
-
-let counters = [8, 8, 8, 8, 8, 8];
-const populateFaces = () => {
-  for (let i = 0; i < 6; i++) {
-    for (let j = 0; j < 9; j++) {
-      if (j !== 4) {
-        do {
-          color = Math.floor(Math.random() * 6);
-        } while (counters[color] === 0);
-        faces[i][j] = colors[color];
-        counters[color]--;
+const cubeFactory = () => ({
+  colors: ["Y", "W", "B", "G", "R", "O"],
+  faces: [[], [], [], [], [], []],
+  populateFaces() {
+    let counters = [8, 8, 8, 8, 8, 8];
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 9; j++) {
+        if (j === 4) {
+          this.faces[i].push(this.colors[i]);
+        } else {
+          let color;
+          do {
+            color = Math.floor(Math.random() * 6);
+          } while (counters[color] === 0);
+          this.faces[i].push(this.colors[color]);
+          counters[color]--;
+        }
       }
     }
-  }
-};
+  },
+  turnFace(n, clockwise) {
+    let newFace = [];
+    newFace[4] = this.faces[n][4];
+    if (clockwise) {
+      newFace[0] = this.faces[n][6];
+      newFace[1] = this.faces[n][3];
+      newFace[2] = this.faces[n][0];
+      newFace[3] = this.faces[n][7];
+      newFace[5] = this.faces[n][1];
+      newFace[6] = this.faces[n][8];
+      newFace[7] = this.faces[n][5];
+      newFace[8] = this.faces[n][2];
+    } else {
+      newFace[0] = this.faces[n][2];
+      newFace[1] = this.faces[n][5];
+      newFace[2] = this.faces[n][8];
+      newFace[3] = this.faces[n][1];
+      newFace[5] = this.faces[n][7];
+      newFace[6] = this.faces[n][0];
+      newFace[7] = this.faces[n][3];
+      newFace[8] = this.faces[n][6];
+    }
+    this.faces[n] = newFace;
+  },
+});
 
-const turnFace = (face, clockwise) => {
-  let newFace = [];
-  newFace[4] = face[4];
-  if (clockwise) {
-    newFace[0] = face[6];
-    newFace[1] = face[3];
-    newFace[2] = face[0];
-    newFace[3] = face[7];
-    newFace[5] = face[1];
-    newFace[6] = face[8];
-    newFace[7] = face[5];
-    newFace[8] = face[2];
-  } else {
-    newFace[0] = face[2];
-    newFace[1] = face[5];
-    newFace[2] = face[8];
-    newFace[3] = face[1];
-    newFace[5] = face[7];
-    newFace[6] = face[0];
-    newFace[7] = face[3];
-    newFace[8] = face[6];
-  }
-  return newFace;
-};
+let cube = cubeFactory();
 
-populateFaces();
-console.log(faces);
-faces[5] = turnFace(faces[5], false);
-console.log(faces);
+cube.populateFaces();
+console.log(cube.faces);
+cube.turnFace(5, true);
+console.log(cube.faces);
